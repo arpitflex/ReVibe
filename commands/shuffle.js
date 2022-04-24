c4042461fac70a4d2ae80d5495689b8e74ef1fd4
Arpit Agrawal <a.agrawal-5@student.tudelft.nl>
2022-04-25 00:04:45 +0200

    Implement shuffle

---
+++ commands/shuffle.js
@@ -0,0 +1,20 @@
+const {SlashCommandBuilder} = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('shuffle')
+        .setDescription('Shuffle the queue!'),
+    async execute(interaction, client, player) {
+        if (!interaction.member.voice.channelId) {
+            return await interaction.reply({content: 'You are not in a voice channel', ephemeral: true});
+        }
+        if (interaction.guild.me.voice.channelId && interaction.member.voice.channelId !== interaction.guild.me.voice.channelId) {
+            return await interaction.reply({content: 'You are not in my voice channel', ephemeral: true});
+        }
+
+        const queue = player.getQueue(interaction.guildId);
+        await interaction.deferReply();
+        queue.shuffle();
+        return await interaction.followUp({content: `:twisted_rightwards_arrows: | **Shuffled!**`});
+    }
+}
